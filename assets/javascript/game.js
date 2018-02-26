5e6863ab66be0010f554cb0f58f17ac1438b9c62
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-02-26 16:53:25 -0500

    hangman update

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+// Pick random word
+var words = ["bracket",
+"final four",
+"ncaa",
+"elite eight",
+"sweet sixteen",
+"upset",
+"tournament",
+"basketball",
+"college"];
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+// Set up answer
+var answer = [];
+
+for (var i = 0; i < word.length; i++) {
+    answer[i] = "_";
+}
+
+var remianingLetters = word.length;
+
+// main loop
+while (remianingLetters > 0) {
+
+    // Show progress
+
+
+    //Take player guess
+
+
+}
+
+
+// Ensure player guess is a valid letter 
+
+// Track guessed letters
+var guessedLetters = [];
+
+// Finish if word is guessed
+
+
+
+
+
+
+
+
