648eb0f1b1faa4036117b05de4654324bdc1f301
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-03-01 18:00:22 -0500

    hangman update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,149 @@
-// Pick random word
+// Array of words 
 var words = ["bracket",
-"final four",
+"tarheels",
 "ncaa",
-"elite eight",
-"sweet sixteen",
+"kentucky",
+"duke",
 "upset",
 "tournament",
 "basketball",
-"college"];
+"college",
+"villanova",
+"louisville",
+"connecticut",
+"kansas"];
 
-var word = words[Math.floor(Math.random() * words.length)];
+// answer
+var answer = '';
+var answerDisplay = [];
 
-// Set up answer
-var answer = [];
+// wins /losses/ remaining guesses
+var wins = 0;
+var winCounter= 0;
+var losses = 0;
+var remainingGuesses = 15;
 
-for (var i = 0; i < word.length; i++) {
-    answer[i] = "_";
+// letters
+var letterList = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+// other variables
+var missedGuesses = [];
+var userGuess;
+var remianingLetters;
+
+
+// Functions
+
+function getWord() {
+
+    // pick random word
+    answer = words[Math.floor(Math.random() * words.length)];
+    console.log(answer);
+
+    // Set up answer after word is picked
+    for (var i = 0; i < answer.length; i++) {
+        answerDisplay.push('_');
+    }
+
 }
 
-var remianingLetters = word.length;
+// Restart when game is over
+function restartGame () {
+    getWord();
+    remainingGuesses = 15;
+    missedGuesses = [];
+    // answerDisplay = [];
+}
 
-// main loop
-while (remianingLetters > 0) {
+// wins/losses
+function gameResults(){
+    if (winCounter === answer.length) {
+        alert("Nice win! Keep going for a National Championship!")
+        wins++;
+        getWord();
+    }
+    else if (remainingGuesses === 0) {
+            alert("You have been eliminated! Better luck next time!");
+            losses++;
+            getWord();
+        }
+}
 
-    // Show progress
+// Show progress
+function update(){
+    for (var j = 0; j < answer.length; j++) {
+        if (answer[j] === userGuess){
+            answerDisplay[j] = userGuess;
+            console.log(answerDisplay);
+            winCounter++;
+            gameResults();
+        }   
+    }
+}
 
 
-    //Take player guess
+// Display to HTML
+getWord();
+document.querySelector("#wins").innerHTML = ""  + wins;
+document.querySelector("#losses").innerHTML = ""  + losses;
+document.querySelector("#remainingGuesses").innerHTML = ""  + remainingGuesses;
+document.querySelector("#gameBoard").innerHTML = answerDisplay.join(" ");
 
+//Main function
+
+// This function is run whenever the user presses a key
+
+//Take player guess
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed
+    userGuess = event.key;
+    console.log(userGuess);
+
+    // Determines if guess is in the answer
+    if (answer.indexOf(userGuess) > -1) {
+        update();
+        console.log("correct letter");
+    }
+    else {
+        missedGuesses.push(userGuess);
+        console.log(missedGuesses);
+        remainingGuesses--;
+        gameResults();
+    }
+
+    // Ensure player guess is a valid letter 
+    if (letterList.indexOf(userGuess) === -1){
+        alert ("Foul! LETTERS ONLY! The other team is shooting free throws!");
+    }
+
+    // if (remainingGuesses === 10) {
+    //     alert("There's still plenty of time on the clock!");
+    // }
+
+    // if (remainingGuesses === 5) {
+    //     alert("Halftime is over! You need to make shots!");
+    // }
+
+    // if (remainingGuesses === 2) {
+    //     alert("It's a close game! You can do it!");
+    // }
+
+    document.querySelector("#wins").innerHTML = ""  + wins;
+    document.querySelector("#losses").innerHTML = ""  + losses;
+    document.querySelector("#remainingGuesses").innerHTML = ""  + remainingGuesses;
+    document.querySelector('#incorrectLetters').innerHTML = missedGuesses.join(" , ");
+    document.querySelector("#gameBoard").innerHTML = answerDisplay.join(" ");
 
 }
 
 
-// Ensure player guess is a valid letter 
 
-// Track guessed letters
-var guessedLetters = [];
+    
 
-// Finish if word is guessed
+  
+
+
 
 
 
